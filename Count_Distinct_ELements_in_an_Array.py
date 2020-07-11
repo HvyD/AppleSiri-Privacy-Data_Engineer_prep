25b6a534e66ece1299bc9986a629f39d8a608d6a
HvyD <zscottywd@msn.com>
2020-07-11 03:12:00 -0700

    Refactor: Added for Cluster

--- Count_Distinct_ELements_in_an_Array.py
+++ Count_Distinct_ELements_in_an_Array.py
@@ -1,17 +1,19 @@
 
-1. First  ask for basic simple solution
-# O(NLogN)
+1. First I gave a basic Solution
 
+# O(NLogN)
 from collections import Counter
 
 def countDistinct(arr):
     return len(Counter(arr).keys())
 
 arr = [10, 20, 20, 10, 20]
-
 print(countDistinct(arr))  # => 2
 
-2. Then ask for solution with better efficiency
+
+
+2. Then I was ask for a better solution
+
 # O(N) - using hashing more efficiently
 def countDistinct(arr):
     n = len(arr)
@@ -24,15 +26,16 @@ def countDistinct(arr):
     return res
 
 arr = [6, 10, 5, 4, 9, 120, 4, 6, 10]
-
 print(countDistinct(arr))  # => 6
 
 
-3. Then ask how this can be done on a Big Scale
+3. Then ask how this can be done across a cluster of 3
 
-1.arr = [1,4,3,2,5,2,1,3,2,1,4,2,1] => 5
-# shuffle/Split
-2a. [1,1,1,1] => 1
-2b. [2,2,2,2] => 1
-2c.[3,3,4,4,5] => 3
-3. a+b+c => 5
+# Given The array below across three clusters to still return The Count of Distinct Elements:
+1.arr = [1,4,3,2,5,2,1,3,2,1,4,2,1]  #=> 5
+# shuffle/Split acroos three clusters
+2a. [1,1,1,1] #=> 1
+2b. [2,2,2,2] #=> 1
+2c.[3,3,4,4,5] #=> 3
+# return The Count of Distinct Elements
+3. a+b+c #=> 5
